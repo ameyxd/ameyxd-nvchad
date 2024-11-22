c44c89944d4e4d3cf36be2e59801115bc676f1c7
Amey Ambade <ameyambade@gmail.com>
2024-11-21 21:05:04 -0600

    Add personal config options

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,37 @@
 require "nvchad.options"
 
--- add yours here!
+local opt = vim.opt
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- Better UI
+opt.number = true -- Show line numbers
+opt.relativenumber = true -- Show relative line numbers
+opt.cursorline = true -- Highlight current line
+opt.signcolumn = "yes" -- Always show sign column
+opt.termguicolors = true -- True color support
+opt.scrolloff = 8 -- Lines of context
+opt.sidescrolloff = 8 -- Columns of context
+
+-- Better editing experience
+opt.expandtab = true -- Use spaces instead of tabs
+opt.smartindent = true -- Insert indents automatically
+opt.wrap = false -- No line wrapping
+opt.ignorecase = true -- Ignore case when searching
+opt.smartcase = true -- Don't ignore case with capitals
+opt.clipboard = "unnamedplus" -- Use system clipboard
+
+-- Better folding
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldlevel = 99
+
+-- Better UI experience
+opt.pumheight = 10 -- Maximum number of items to show in popup menu
+opt.showmode = false -- Don't show mode in command line
+opt.splitbelow = true -- Horizontal splits will be below
+opt.splitright = true -- Vertical splits will be to the right
+opt.timeoutlen = 300 -- Time to wait for mapped sequence to complete (in milliseconds)
+
+-- Better experience with files
+opt.undofile = true -- Enable persistent undo
+opt.updatetime = 250 -- Faster completion
+opt.writebackup = false -- Don't make backup before overwriting file
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,7 +5,6 @@ return {
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,13 +12,160 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  -- Better UI elements
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+      },
+    },
+  },
+
+  -- File tree with git status
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = "Neotree",
+  },
+
+  -- Better diagnostics list and others
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    opts = { use_diagnostic_signs = true },
+  },
+
+  -- Git integration
+  {
+    "lewis6991/gitsigns.nvim",
+    lazy = false,
+    opts = {
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "󰍵" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "│" },
+      },
+    },
+  },
+
+  -- Autoclose tags and brackets
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    opts = {},
+  },
+
+  -- Better indentation
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+  },
+
+  -- Which-key for keybinding help
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {},
+  },
+
+  -- Enable treesitter for better syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css", "javascript",
+        "typescript", "tsx", "json",
+        "python", "markdown", "markdown_inline",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      autotag = { enable = true },
+    },
+  },
+
+  -- Mini plugins collection (add after other plugins)
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = {
+      cursor = { enable = true },
+      scroll = { enable = true },
+    },
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+  },
+
+  -- Better quickfix window
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "VeryLazy",
+    opts = {
+      keywords = {
+        FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+        TODO = { icon = " ", color = "info" },
+        HACK = { icon = " ", color = "warning" },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+      },
+    },
+  },
+
+  -- Better code navigation
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    },
+  },
 }
